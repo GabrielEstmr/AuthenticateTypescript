2a14f964da51268c9b254ed8cb8c4b20ce52b967
Gabriel Rodrigues <gabriel.rodrigues@obuc.com.br>
2021-02-24 18:59:59 -0300

    first commit

---
+++ src/shared/container/index.ts
@@ -0,0 +1,23 @@
+import { container } from 'tsyringe';
+
+import '@modules/users/providers';
+import './providers/'
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import UsersRepository from '@modules/users/infra/typeorm/Repositories/UserRepository';
+
+
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/Repositories/UserTokensRepository';
+
+
+container.registerSingleton<IUsersRepository>(
+    'UsersRepository',
+    UsersRepository
+);
+
+container.registerSingleton<IUserTokensRepository>(
+    'UserTokensRepository',
+    UserTokensRepository
+);
+
